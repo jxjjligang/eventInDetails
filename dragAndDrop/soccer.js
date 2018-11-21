e614ea28326eea85cd64ff6041c0b29f37c89f26
Gang Li <jxjjligang@gmail.com>
2018-11-21 15:29:25 -0700

    Make improvements to drag superhero task.

--- dragAndDrop/soccer.js
+++ dragAndDrop/soccer.js
@@ -1,4 +1,5 @@
 let draggedElem;
+body.addEventListener('mousemove', onBodyMousemove);
 
 body.onmousedown = function (event) {
     event.preventDefault();
@@ -9,20 +10,50 @@ body.onmousedown = function (event) {
         let rect = event.target.getBoundingClientRect();
         draggedElem = {
             target: event.target,
-            shiftX: event.clientX + window.pageXOffset - rect.x,
-            shiftY: event.clientY + window.pageYOffset - rect.y
+            shiftX: event.clientX - rect.x,
+            shiftY: event.clientY - rect.y
         }
+        console.log(draggedElem);
     }
 }
 
-body.onmousemove = function (event) {
-    if (draggedElem) {
-        draggedElem.target.style.position = 'absolute';
-        draggedElem.target.style.left = (event.clientX - draggedElem.shiftX) + 'px';
-        draggedElem.target.style.top = (event.clientY - draggedElem.shiftY) + 'px';
+function onBodyMousemove(event) {
+    if (!draggedElem || event.target !== draggedElem.target)
+        return;
+
+    if (event.clientY < 0 || event.clientY > window.innerHeight)
+        console.log(`mousemove clientY:${event.clientY}`);
+
+    draggedElem.target.style.position = 'absolute';
+    draggedElem.target.style.left = (event.clientX - draggedElem.shiftX + window.pageXOffset) + 'px';
+
+    let yScrollBy = 0;
+    let newTop = event.clientY - draggedElem.shiftY;
+    if (newTop < 0) {
+        console.log(`newTop:${newTop}, elementTop:${window.pageYOffset}px`);
+        newTop = 0;
+        yScrollBy = -10;
     }
+    else if (newTop + event.target.offsetHeight > (window.innerHeight)) {
+        newTop = window.innerHeight - event.target.offsetHeight;
+        yScrollBy = 10;
+    }
+
+    draggedElem.target.style.top = (newTop + window.pageYOffset) + 'px';
+    if (yScrollBy !== 0)
+        window.scrollBy(0, yScrollBy);
 }
 
 body.onmouseup = function (event) {
     draggedElem = undefined;
+}
+
+body.onmouseout = function (event) {
+    if (event.target !== body)
+        return;
+
+    if (!event.target.contains(event.relatedTarget)) {
+        draggedElem = null;
+        // console.log(event);
+    }
 }
